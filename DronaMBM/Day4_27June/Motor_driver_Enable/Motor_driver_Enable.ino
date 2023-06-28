bc42efccedf91682df3edd4be22b1a0d9cb2dc00
BHANWARLAL PRAJAPAT <85385314+BHANWARLALPRAJAPAT@users.noreply.github.com>
2023-06-29 00:42:49 +0530

    Motor_driver_Enable.ino

--- DronaMBM/Day4_27June/Motor_driver_Enable/Motor_driver_Enable.ino
+++ DronaMBM/Day4_27June/Motor_driver_Enable/Motor_driver_Enable.ino
@@ -1,59 +1,65 @@
+//Assignment : Experimentation with PWM controlled N20 DC MOTOR using Arduino UNO
 //Author : BHANWARLAL PRAJAPAT
 //Date : 27 June 2023
 #define MOTOR_INPUT_PIN_A 4
 #define MOTOR_INPUT_PIN_B 5
 #define MOTOR_ENABLE_PIN 3
 void setup() {
+  Serial.begin(9600);
   pinMode(MOTOR_INPUT_PIN_A, OUTPUT);
   pinMode(MOTOR_INPUT_PIN_B, OUTPUT);
   pinMode(MOTOR_ENABLE_PIN, OUTPUT);
-  RotateMotor();
-  SpeedMotor();
-  
 }
-void RotateMotor()
-{
+void RotateMotor() {
+  //Turn Motor in ACW direction
   digitalWrite(MOTOR_INPUT_PIN_A, HIGH);
   digitalWrite(MOTOR_INPUT_PIN_B, LOW);
+  analogWrite(MOTOR_ENABLE_PIN, 255);
   delay(2000);
-
+  
+  //Stop rotaion of Motor
   digitalWrite(MOTOR_INPUT_PIN_A, LOW);
   digitalWrite(MOTOR_INPUT_PIN_B, LOW);
-  delay(2000);
+  analogWrite(MOTOR_ENABLE_PIN, LOW);
+  delay(1000);
 
+  //Turn Motor in CW direction
   digitalWrite(MOTOR_INPUT_PIN_A, LOW);
   digitalWrite(MOTOR_INPUT_PIN_B, HIGH);
+  analogWrite(MOTOR_ENABLE_PIN, 255);
   delay(2000);
 
-  digitalWrite(MOTOR_INPUT_PIN_A,LOW);
-  digitalWrite(MOTOR_INPUT_PIN_B,LOW);
-  delay(2000);
+  //Stop rotaion of Motor
+  digitalWrite(MOTOR_INPUT_PIN_A, LOW);
+  digitalWrite(MOTOR_INPUT_PIN_B, LOW);
+  analogWrite(MOTOR_ENABLE_PIN, LOW);
+  delay(1000);
 }
-void SpeedMotor()
-{
-  //Turn On the Motor
-  digitalWrite(MOTOR_INPUT_PIN_A,LOW);
-  digitalWrite(MOTOR_INPUT_PIN_B,LOW);
-  dalay(10);
+void SpeedMotor() {
+  //stop Rotation of Motor for 10 ms
+  digitalWrite(MOTOR_INPUT_PIN_A, LOW);
+  digitalWrite(MOTOR_INPUT_PIN_B, LOW);
+  delay(10);
 
-  digitalWrite(MOTOR_INPUT_PIN_A,LOW);
-  digitalWrite(MOTOR_INPUT_PIN_B,HIGH);
+  //Turn Motor in CW direction
+  digitalWrite(MOTOR_INPUT_PIN_A, LOW);
+  digitalWrite(MOTOR_INPUT_PIN_B, HIGH);
 
-  //ACCERATION 
-  for(int i = 0;i<256;i++)
-  {
-    analogWrite(MOTOR_ENABLE_PIN,i);
-    delay(20);
+  // Acceleration of Motor Rotation
+  for (int i = 0; i < 256; i++) {
+    analogWrite(MOTOR_ENABLE_PIN, i);
+    Serial.print("Speed " + String(i));
+    delay(50);
   }
-
-
-  // De-Accelaration
-  for(int i = 255;i>=0;i--)
-  {
-    analogWrite(MOTOR_ENABLE_PIN,i);
-    delay(20);
+  
+  // Decelaration  of Motor Rotation
+  for (int i = 255; i >= 0; i--) {
+    analogWrite(MOTOR_ENABLE_PIN, i);
+    Serial.print("Speed " + String(i));
+    delay(50);
   }
 }
 void loop() {
-
+  RotateMotor();
+  SpeedMotor();
 }
